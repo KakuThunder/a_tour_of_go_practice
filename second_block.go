85074c7f4eca1b247ee130ffa39d9fee2a7b8b52
kakuthunder <pasuo1292r@gmail.com>
2019-06-16 11:31:41 +0900

    add source second_block

--- second_block.go
+++ second_block.go
@@ -11,7 +11,9 @@ Flow control statements:
 
 import (
 	"fmt"
+	"time"
 	"math"
+	"runtime"
 )
 
 //if statement pattern1
@@ -47,7 +49,11 @@ func main2() {
 
 	fmt.Println("------ start main2 ------")
 
-
+	//defer に設定した関数の実行を関数の終了の直前に行ってくれる
+	//関数の引数は設定した時点での値を評価することを留意しておく
+	//複数ある場合は <LIFO> で実行
+	defer fmt.Println("proc end")
+	defer fmt.Println("process complete")
 
 	//for roop pattern1
 	sum := 0
@@ -84,6 +90,42 @@ func main2() {
 		pow2(3,2,10),
 		pow2(3,3,20),
 	)
+
+	//switch 一番最初に条件を満たすcaseのみを実行する
+	switch os:= runtime.GOOS; os{
+		case "darwin":
+			fmt.Println("OS X.")
+		case "linux":
+			fmt.Println("Linux.")
+		default:
+			//freebsd, openbsd,
+			//plan9, windows...
+			fmt.Printf("%s.\n",os)
+	}
+
+	today := time.Now().Weekday()
+	switch time.Saturday {
+		case today + 0:
+			fmt.Println("Today.")
+		case today + 1:
+			fmt.Println("Tommorow.")
+		case today + 2:
+			fmt.Println("In too days.")
+		default:
+			fmt.Println("Too far away.")
+	}
+
+	//一番最初のcaseのみ実行されるので
+	//XX以上,XX以下が簡単に記載できる
+	t := time.Now()
+	switch {
+		case t.Hour() < 12:
+			fmt.Println("Good morning!")
+		case t.Hour() < 17:
+			fmt.Println("Good afternoon!")
+		default:
+			fmt.Println("Good evening!")
+	}
 
 
 }
