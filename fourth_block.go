752d7691a8ac1ce39e9fadf9422c782f1e1b595c
kakuthunder <pasuo1292r@gmail.com>
2019-06-20 22:30:03 +0900

    add fourth_block

---
+++ fourth_block.go
@@ -0,0 +1,59 @@
+package main
+
+/*
+Methods and interfaces
+*/
+
+import (
+	"fmt"
+	"math"
+)
+
+
+type Point struct {
+	X, Y float64
+}
+/* Methods */
+func (v Point) Absm() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+/* Pointer receivers */
+//
+func (v *Point) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f	
+}
+
+
+
+/* function */
+func Absf(v Point) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+/* Methods continued */
+type MyFloat float64
+
+func (f MyFloat) Absmc() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+
+func main4(){
+
+	fmt.Println("------ start main4 ------")
+
+	v := Point{3,4}
+	fmt.Println(v.Absm())
+	fmt.Println(Absf(v))
+
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.Absmc())
+
+	v.Scale(10)
+	fmt.Println(v.Absm())
+
+}
